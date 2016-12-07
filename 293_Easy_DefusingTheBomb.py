72c39e52a5f86434fd2c3198595f93728400ba97
kbake <kbake@outlook.com>
2016-12-07 02:27:57 -0500

    293_Easy solution (did it the other day)

---
+++ 293_Easy_DefusingTheBomb.py
@@ -0,0 +1,23 @@
+WIRE_RULES = {
+    "white":  ["red", "orange", "green", "purple"],
+    "red":    ["green"],
+    "black":  ["red", "black", "purple"],
+    "orange": ["red", "black"],
+    "green":  ["orange", "white"],
+    "purple": ["red", "black"]
+    }
+def is_valid_sequence(wire_sequence):
+    is_valid = True
+    wire_sequence = wire_sequence.split('\n')
+    prev_wire = wire_sequence[0]
+    for wire in wire_sequence[1:]:
+        if wire not in WIRE_RULES[prev_wire]:
+            is_valid = False
+            break
+        prev_wire = wire
+    return is_valid
+INPUT1 = """white\nred\ngreen\nwhite"""
+INPUT2 = """white\norange\ngreen\nwhite"""
+GET_FLAVOR_TEXT = lambda is_valid: "Bomb defused" if is_valid else "Boom"
+print(GET_FLAVOR_TEXT(is_valid_sequence(INPUT1)))
+print(GET_FLAVOR_TEXT(is_valid_sequence(INPUT2)))
